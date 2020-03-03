eafdc905428c63cc86e6fd1961693b54aabf0e24
Wellington Pessoa de Lima Filho <wellplf@gmail.com>
2020-03-03 01:30:20 -0300

    Alteração (number_1) na atividade Vai_e_Volta

---
+++ VaieVolta/app/src/main/java/com/example/vaievolta/Interesting.kt
@@ -0,0 +1,12 @@
+package com.example.vaievolta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Interesting : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_interesting)
+    }
+}
---
+++ VaieVolta/app/src/main/java/com/example/vaievolta/TelaSobre.kt
@@ -0,0 +1,12 @@
+package com.example.vaievolta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaSobre : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_sobre)
+    }
+}
