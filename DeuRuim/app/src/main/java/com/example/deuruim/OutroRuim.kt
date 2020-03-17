51e4790a0d81f52622e33d21629c5ae2caf100df
Wellington Pessoa de Lima Filho <wellplf@gmail.com>
2020-03-17 11:57:41 -0300

    Atualizado

--- DeuRuim/app/src/main/java/com/example/deuruim/OutroRuim.kt
+++ DeuRuim/app/src/main/java/com/example/deuruim/OutroRuim.kt
@@ -2,11 +2,15 @@ package com.example.deuruim
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.SeekBar
+import androidx.core.view.drawToBitmap
 
 class OutroRuim : AppCompatActivity() {
 
@@ -14,6 +18,10 @@ class OutroRuim : AppCompatActivity() {
     private lateinit var sbNota: SeekBar
     private lateinit var btSalvar: Button
     private lateinit var btCancelar: Button
+    private lateinit var btFoto: Button
+    private lateinit var imgvFoto: ImageView
+
+    val PICTURE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +32,10 @@ class OutroRuim : AppCompatActivity() {
 
         this.btSalvar = findViewById(R.id.btFormSalvar)
         this.btCancelar = findViewById(R.id.btFormCancelar)
+        this.btFoto = findViewById(R.id.btFoto)
 
         this.btSalvar.setOnClickListener({ salvar() })
+        this.btFoto.setOnClickListener({ foto() })
         this.btCancelar.setOnClickListener({
             finish()
         })
@@ -34,8 +44,9 @@ class OutroRuim : AppCompatActivity() {
     fun salvar(){
         val descricao = this.etDescricao.text.toString()
         val nota = this.sbNota.progress
+        val foto = this.imgvFoto.drawToBitmap()
 
-        val evento = Evento(descricao, nota)
+        val evento = Evento(descricao, nota, foto)
 
         val intent = Intent()
         intent.putExtra("EVENTO", evento)
@@ -43,5 +54,19 @@ class OutroRuim : AppCompatActivity() {
         setResult(Activity.RESULT_OK, intent)
 
         finish()
+    }
+
+    fun foto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, PICTURE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK && requestCode == PICTURE) {
+            val imagem = data?.extras?.get("data") as Bitmap
+            this.imgvFoto.setImageBitmap(imagem)
+        }
     }
 }
